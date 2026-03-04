7e4a6f3186fde547dfef8986b42871aaeec49ba1
Horus <horus@gokoncentrate.com>
2026-03-04 23:51:29 +0000

    Match vanilla layout exactly: white sidebar, teal accent instead of purple, clean CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>@yield('title') &mdash; {{ config('app.name') }}</title>
     @include('includes.css')
     @stack('style')
-    <link rel="stylesheet" href="{{ asset("css/custom.css?v=1772667954") }}">
+    <link rel="stylesheet" href="{{ asset("css/custom.css?v=1772668289") }}">
 </head>
 
 <body>
