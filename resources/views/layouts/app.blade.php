9a57344e5c1f9f5ee1f01e295483641a5092e19a
Horus <horus@gokoncentrate.com>
2026-03-04 20:23:50 +0000

    Clean header: remove duplicate language dropdown, fix avatar, remove dummy content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,31 +18,29 @@
             <nav class="navbar navbar-expand-lg main-navbar">
                 <form class="form-inline mr-auto">
                     <ul class="navbar-nav mr-3">
-                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars" style="color: #94A3B8;"></i></a></li>
                     </ul>
                 </form>
                 <ul class="navbar-nav navbar-right">
-                    <!-- Language Switcher and User Profile in same line -->
-                    <li class="nav-item d-flex align-items-center">
-                        @include('components.language-switcher')
-                        
-                        <!-- User Profile Dropdown -->
-                        <div class="dropdown ml-2">
-                            <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                                <img alt="image" src="{{ auth()->user()->profile ?? asset('img/avatar/avatar-1.png') }}" class="rounded-circle mr-1" style="width: 30px; height: 30px; object-fit: cover;">
-                                <div class="d-sm-none d-lg-inline-block"> Hi, {{ auth()->user()->name }} </div>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right">
-                                <a href="{{ route('admin.profile') }}" class="dropdown-item has-icon">
-                                    <i class="far fa-user"></i> {{ __('Profile') }} </a>
-                                <div class="dropdown-divider"></div>
-                                <a href="{{ route('admin.logout') }}" class="dropdown-item has-icon text-danger">
-                                    <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }} </a>
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                            <div style="background: #0D9488; color: white; width: 34px; height: 34px; border-radius: 50%; display: inline-flex; align-items: center; justify-content: center; font-weight: 600; font-size: 14px; margin-right: 8px;">
+                                {{ strtoupper(substr(auth()->user()->name ?? 'A', 0, 1)) }}
                             </div>
+                            <div class="d-sm-none d-lg-inline-block" style="color: #E2E8F0;">{{ auth()->user()->name ?? 'Admin' }}</div>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <div class="dropdown-title">{{ auth()->user()->email ?? '' }}</div>
+                            <div class="dropdown-divider"></div>
+                            <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
+                               href="{{ route('admin.logout') }}">
+                                <i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}
+                            </a>
                         </div>
                     </li>
                 </ul>
             </nav>
+            
 
             <!-- Sidebar -->
             @include('components.sidebar')
